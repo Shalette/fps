7f73de6679dcc60a7c6a816e8f54ae43d65c3760
Shalette D'Souza <shalettdsouza@gmail.com>
2019-12-10 23:55:07 +0530

    Changed UI of Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,24 +11,28 @@
     @if(!$publications->isEmpty())
         <h4 class="center-align grey-text text-darken-3">List of Publications</h4>
         <div class="divider"></div>
+        <table class="highlight">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th >Possible Actions</th>
+            </tr>
+        </thead>
+        <tbody>
         @foreach ($publications as $pub)
-            <div class="card-panel grey lighten-5 z-depth-1">
-                <div class="row card-content">
-                    <div class="col s12 m6 l7 xl8">
-                        <h5 class="card-title">{{ $pub->title }}</h5>
-                        <h6 class="grey-text text-darken-2">{{ $pub->description }}</h6>  
-                        <br>  
-                    </div>
-                    
-                    <div class="col s12 m6 l5 xl4">
-                        <a class="col s6 btn blue-grey darken-1 waves-effect waves-light" target="_blank" href="{{ asset('storage/publications/'.$pub->pdf_link )}}">
+            <tr data-href="{{ asset('storage/publications/'.$pub->pdf_link )}}">
+                <td><p class="card-title cyan-text text-darken-4">{{ $pub->title }}</p></td>
+                <td><p class="cyan-text text-darken-2">{{ $pub->description }}</p></td>
+                <td><a class="btn col s12 m5 blue-grey darken-1 waves-effect waves-light" target="_blank" href="{{ asset('storage/publications/'.$pub->pdf_link )}}">
                         <i class="material-icons left">open_in_new</i>View</a> 
-                        <a href="{{ route('edit', $pub->pub_id) }}" class="col s5 offset-s1 btn amber darken-1 waves-effect waves-light">
-                        <i class="material-icons left">mode_edit</i>Edit</a>
-                    </div>
-                </div>
-            </div>
+                <a href="{{ route('edit', $pub->pub_id) }}" class="btn col s12 offset-m2 m5 amber darken-1 waves-effect waves-light">
+                        <i class="material-icons left">mode_edit</i>Edit</a></td>
+            </tr>
+
         @endforeach
+        </tbody>
+        </table>
     @else
         <div class="card-panel center-align">
             <div class="card-body">
