38ff918b07c853592913df745143ef04b243650b
Shalette D'Souza <shalettdsouza@gmail.com>
2019-12-11 00:06:17 +0530

    Changed UI of Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,26 +12,25 @@
         <h4 class="center-align grey-text text-darken-3">List of Publications</h4>
         <div class="divider"></div>
         <table class="highlight">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th >Possible Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach ($publications as $pub)
-            <tr data-href="{{ asset('storage/publications/'.$pub->pdf_link )}}">
-                <td><p class="card-title cyan-text text-darken-4">{{ $pub->title }}</p></td>
-                <td><p class="cyan-text text-darken-2">{{ $pub->description }}</p></td>
-                <td><a class="btn col s12 m5 blue-grey darken-1 waves-effect waves-light" target="_blank" href="{{ asset('storage/publications/'.$pub->pdf_link )}}">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Possible Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($publications as $pub)
+                <tr data-href="{{ asset('storage/publications/'.$pub->pdf_link )}}">
+                    <td><p class="card-title cyan-text text-darken-4">{{ $pub->title }}</p></td>
+                    <td><p class="cyan-text text-darken-2">{{ $pub->description }}</p></td>
+                    <td><a class="btn col s12 m5 blue-grey darken-1 waves-effect waves-light" target="_blank" href="{{ asset('storage/publications/'.$pub->pdf_link )}}">
                         <i class="material-icons left">open_in_new</i>View</a> 
-                <a href="{{ route('edit', $pub->pub_id) }}" class="btn col s12 offset-m2 m5 amber darken-1 waves-effect waves-light">
+                    <a href="{{ route('edit', $pub->pub_id) }}" class="btn col s12 offset-m2 m5 amber darken-1 waves-effect waves-light">
                         <i class="material-icons left">mode_edit</i>Edit</a></td>
-            </tr>
-
-        @endforeach
-        </tbody>
+                </tr>
+            @endforeach
+            </tbody>
         </table>
     @else
         <div class="card-panel center-align">
